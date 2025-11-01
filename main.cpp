41ae83ad715abe2a1b9af75de67e3aea94db0f84
JuanGiraldoSoftware <juangiraldosoftware@gmail.com>
2025-10-31 21:35:14 -0500

    GWP-C003 | chore: First canvas version plus implementing primitive mechanics.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,38 @@ extern "C" {
     #include "raylib.h"
 }
 
-int main()
-{
-    InitWindow(800, 450, "Hello Raylib!");
+void EventPlayerMove(Vector2& dPlayerPos){
 
+    if (IsKeyDown(KEY_D)) dPlayerPos.x+=2.0f;
+    if (IsKeyDown(KEY_A)) dPlayerPos.x-=2.0f;
+    if (IsKeyDown(KEY_W)) dPlayerPos.y-=2.0f;
+    if (IsKeyDown(KEY_S)) dPlayerPos.y+=2.0f;
+
+}
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "Hello Raylib!");
+
+    Vector2 playerPos = { (float)screenWidth/2, (float)screenHeight/2 }; // Screen center
+    Vector2 resourcePos = { (float)screenWidth/2 + 20, (float)screenHeight/2 + 20};
+
+    SetTargetFPS(60); //Setting game frames-per-second dif.
+    
     while (!WindowShouldClose())
     {
+        EventPlayerMove(playerPos);
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
 
-        DrawText("Hello World from Raylib!", 190, 200, 20, BLACK);
-
+            ClearBackground(RAYWHITE);
+            //Player Updater
+            DrawCircleV(playerPos, 10, BLACK);
+            //Resources
+            DrawCircleV(resourcePos, 7, RED);
+        
         EndDrawing();
     }
 
